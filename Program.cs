ce8c1718975c5b0fe8c3714ad14e28c93ba35291
gregpec <gregpec@yahoo.com>
2024-05-23 17:21:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using MotoApp;
+
+var stack = new BasicStack<double>();
+stack.Push(45.3);
+stack.Push(124);
+stack.Push(546.1);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($" item {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum:{sum}");
+
+var stackString = new BasicStack<string>();
+stackString.Push("Company A");
+stackString.Push("Company B");
+stackString.Push("Company C");
+
+while (stackString.Count > 0)
+{
+    string item = stackString.Pop();
+    Console.WriteLine($" item {item}");
+}
